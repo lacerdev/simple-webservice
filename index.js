c7c3a8f8c1d7eacfb379dfcbbcfd4899bc2cdced
André Lacerda <lacerdev@gmail.com>
2021-11-30 11:23:15 -0300

    fix: fixing port error

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import server from "./src/server.js";
+const port = process.env.PORT || 3000;
 
-server.listen(process.env.PORT || 3000, () => {
+server.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
