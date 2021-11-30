d75c6230da981660e331d5fd12fdf17609824473
André Lacerda <lacerdev@gmail.com>
2021-11-30 11:03:15 -0300

    feat: adding "/" endpoint

--- src/server.js
+++ src/server.js
@@ -2,6 +2,11 @@ import express from "express";
 
 const app = express();
 
+app.get("/", (request, response) => {
+  response.write("A Simple WebService");
+  response.end();
+});
+
 app.get("/healthy", (request, response) => {
   response.json("I'm in good health");
 });
