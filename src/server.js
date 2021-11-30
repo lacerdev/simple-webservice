e48f557f50951e4fa38190d9abd8682fa3ab5aba
André Lacerda <lacerdev@gmail.com>
2021-11-30 11:56:05 -0300

    fix: fixing timezone of logs

--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ const options = {
   year: 'numeric', month: 'numeric', day: 'numeric',
   hour: 'numeric', minute: 'numeric', second: 'numeric',
   hour12: false,
+  timeZone: "America/Sao_Paulo"
 };
 // app.use(express.static("public"))
 app.use(express.json());
