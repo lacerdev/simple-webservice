04562c9b16e3080f8213c1fa4c8597b259333e62
André Lacerda <lacerdev@gmail.com>
2021-11-30 11:37:19 -0300

    feat: adding /get e /post endpoints

--- src/server.js
+++ src/server.js
@@ -3,9 +3,32 @@ import express from "express";
 const app = express();
 
 // app.use(express.static("public"))
+app.use(express.json());
 
 app.get("/", (request, response) => {
   response.send("<h1>A Simple WebService</h1>");
+});
+
+app.get("/healthy", (request, response) => {
+  response.json("I'm in good health");
+});
+
+app.get("/get", (request, response) => {
+  const { route, query } = request;
+  response.json({
+    route,
+    query,
+  });
+});
+
+app.post("/post", (request, response) => {
+  const { route, query, body } = request;
+
+  response.json({
+    route,
+    query,
+    body,
+  });
 });
 
 app.get("/healthy", (request, response) => {
